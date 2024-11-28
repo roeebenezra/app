90972120b983c5157c553986e1c0f306e0c3daa5
Roee <roibenezra0@gmail.com>
2024-11-28 15:49:53 +0200

    try sticky session

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, make_response
 import socket
+import sys
 import datetime
 import mysql.connector
 from threading import Lock
@@ -18,41 +19,11 @@ db_config = {
     'database' : 'app_db'
 }
 
-# Helper function to initialize database tables
-def initialize_db():
-    try:
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor()
-
-        # Create tables if they don't exist
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS access_log (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            timestamp DATETIME NOT NULL,
-            client_ip VARCHAR(45) NOT NULL,
-            server_ip VARCHAR(45) NOT NULL
-        );
-        """)
-
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS counter (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            value INT NOT NULL
-        );
-        """)
-
-        conn.commit()
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-    finally:
-        cursor.close()
-        conn.close()
-
 # Helper function to get internal IP address
 def get_internal_ip():
     return socket.gethostbyname(socket.gethostname())
 
-# Route: "/"
+
 @app.route("/")
 def index():
     global counter
@@ -88,7 +59,9 @@ def index():
 
         # Record access log
         client_ip = request.remote_addr
+        print('client_ip = ', client_ip, file=sys.stderr)
         server_ip = get_internal_ip()
+        print('server_ip = ', server_ip, file=sys.stderr)
         timestamp = datetime.datetime.now()
 
         cursor.execute(
@@ -102,12 +75,13 @@ def index():
         conn.close()
 
     # Set a cookie with the server's internal IP for 5 minutes
-    response = make_response(f"Internal IP Address of Server: {get_internal_ip()}")
-    response.set_cookie('server_ip', get_internal_ip(), max_age=300)
-
+    server_ip = get_internal_ip()
+    response = make_response(f"Internal IP Address of Server: {server_ip}")
+    response.set_cookie('srv_id', server_ip, max_age=300)
+    
     return response
 
-# Route: "/showcount"
+
 @app.route("/showcount")
 def show_count():
     global counter
